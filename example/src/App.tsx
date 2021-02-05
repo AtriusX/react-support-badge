b1f5525876dfa5f491ab3fdcfbd54a4b9853e417
Atrius <atriusedm@gmail.com>
2021-02-05 14:32:51 -0500

    init create-react-library@3.1.1

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-support-badge'
+import 'react-support-badge/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
