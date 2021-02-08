5f499da8b09293c639408b6f30eb76e95edf9673
Atrius <atriusedm@gmail.com>
2021-02-08 14:57:22 -0500

    Add support bars

---
+++ src/components/SupportBar.tsx
@@ -0,0 +1,85 @@
+import React, { HTMLProps } from 'react'
+import styled, { CSSProperties } from 'styled-components'
+
+const Bar = styled.div`
+  position: fixed;
+  bottom: 0%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  font-weight: 600;
+  padding: 1.5em 1.25em;
+  transition: all 1s;
+  z-index: 1000;
+`
+
+const Text = styled.p`
+  position: relative;
+  display: inline-block;
+  width: 100%;
+  color: white;
+  font-size: 1.1em;
+  padding-left: 1em;
+`
+
+const Button = styled.a`
+  position: relative;
+  right: 1em;
+  margin-right: 1.5em;
+  width: 10em;
+  display: block;
+  text-align: center;
+  padding: 0.5em;
+  color: white;
+  text-decoration: none;
+  filter: hue-rotate(20deg);
+  transition: all 0.2s;
+  user-select: none;
+
+  &:hover {
+    filter: hue-rotate(-20deg);
+  }
+`
+
+export interface BarData extends HTMLProps<HTMLDivElement> {
+  buttonText?: string
+  link: string
+  look?: string
+  buttonLook?: string
+}
+
+export function SupportBar({
+  buttonText,
+  link,
+  look,
+  buttonLook,
+  children,
+  className,
+  style
+}: BarData) {
+  const barStyle: CSSProperties = {
+    background: look || '#0066FF',
+    ...style
+  }
+  const buttonStyle: CSSProperties = {
+    background: buttonLook || barStyle.background
+  }
+  return (
+    <Bar className={className} style={barStyle}>
+      <Text>{children || 'Please consider supporting me!'}</Text>
+      <Button style={buttonStyle} href={link}>
+        {buttonText || 'Support'}
+      </Button>
+      <Button
+        style={buttonStyle}
+        onClick={(e) => {
+          const elem = e.currentTarget.parentElement!
+          elem.style.opacity = '0'
+          elem.style.pointerEvents = 'none'
+        }}
+      >
+        Dismiss
+      </Button>
+    </Bar>
+  )
+}
