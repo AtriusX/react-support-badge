0249254b3a7c79849106a145d099f683922eede7
Atrius <atriusedm@gmail.com>
2021-02-08 14:57:30 -0500

    Add support bubbles

---
+++ src/components/SupportBubble.tsx
@@ -0,0 +1,66 @@
+import React, { HTMLProps } from 'react'
+import styled, { CSSProperties } from 'styled-components'
+
+const Bubble = styled.a`
+  position: fixed;
+  z-index: 1000;
+  text-align: center;
+  padding: 1em 3em;
+  margin: 2em;
+  text-decoration: none;
+  font-size: 1.1em;
+  font-weight: 600;
+  border-radius: 100px;
+  color: white;
+  transition: all 0.2s;
+
+  &:hover {
+    filter: brightness(1.2);
+    transform: translateY(-2px);
+  }
+`
+
+type Position = 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right'
+
+export interface BubbleData extends HTMLProps<HTMLLinkElement> {
+  link: string
+  look?: string
+  position?: Position
+}
+
+export function SupportBubble({
+  link,
+  look,
+  position,
+  className,
+  style
+}: BubbleData) {
+  const data = position?.split('-') || ['bottom', 'right']
+  const vertical =
+    data[0] === 'bottom'
+      ? {
+          bottom: 0
+        }
+      : {
+          top: 0
+        }
+  const horizontal =
+    data[1] === 'right'
+      ? {
+          right: 0
+        }
+      : {
+          left: 0
+        }
+  const bubbleStyle: CSSProperties = {
+    background: look || '#0066FF',
+    ...style,
+    ...vertical,
+    ...horizontal
+  }
+  return (
+    <Bubble className={className} href={link} style={bubbleStyle}>
+      Support
+    </Bubble>
+  )
+}
